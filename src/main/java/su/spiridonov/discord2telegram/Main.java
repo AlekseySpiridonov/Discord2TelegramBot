ed22f0a856ea9382c267a1a0c43d2277cb008c57
Aleksey Spiridonov <aleksey@spiridonov.su>
2023-10-24 20:51:58 +0400

    Printing Bot version in the StartUp Message

--- src/main/java/su/spiridonov/discord2telegram/Main.java
+++ src/main/java/su/spiridonov/discord2telegram/Main.java
@@ -1,7 +1,13 @@
 package su.spiridonov.discord2telegram;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class Main {
+
+    private static Logger logger = LoggerFactory.getLogger(Main.class);
     public static void main(String[] args) {
+        logger.info("Discord2TelegramBot v"+Main.class.getPackage().getImplementationVersion());
         BotRunner botRunner = new BotRunner();
         botRunner.runBots();
     }
